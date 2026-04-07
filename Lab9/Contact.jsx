b628c36a12d037c6c256011ff9a18aee2801a319
Swaroopms08 <swaroopms739@gmail.com>
2026-04-07 12:06:45 +0530

    Create Contact.jsx

---
+++ Lab9/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function Contact() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Contact Us</h1>
+      <p>Email: swaroop.23cs169@sode-edu.in</p>
+      <p>Phone: +91 9008632739</p>
+
+    </div>
+  );
+}
+export default Contact;
